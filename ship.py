e475fdea90623944cfbfa4b4c6a61b6c5df571ce
Kak D-Wing <avehwing@gmail.com>
2019-12-18 16:44:05 +0800

    Update ship.py
    Initiaze settings at ship. Convert self.rect.x (x coordinate) from integer to float. Each movement update is now based on ship speed unit at settings. After the speed increment is done, it is updated to self.rect.x.

--- ship.py
+++ ship.py
@@ -12,6 +12,7 @@ class Ship:
     def __init__(self, ai_game):
         '''Initialize the ship and set its starting position'''
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
         
         # Load the ship image and get its rect
@@ -20,6 +21,9 @@ class Ship:
         
         # Start new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+        
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
         
         # Movement flag
         self.moving_right = False
@@ -32,6 +36,12 @@ class Ship:
     def update(self):
         '''Update the ship's position based on the movement flag.'''
         if self.moving_right:
-            self.rect.x += 1.5
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1.5
+            self.x -= self.settings.ship_speed
+        
+        # Update rect object from self.x.
+        self.rect.x = self.x
+        
+        
+        
