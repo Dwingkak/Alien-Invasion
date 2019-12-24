dd997de3c4a7a6b398bf34fef696fcf159bb8ee9
Kak D-Wing <avehwing@gmail.com>
2019-12-24 08:46:10 +0800

    Create game_stats.py
    Added a game statistics tracker. Can track number of ship left.

---
+++ game_stats.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 24 08:29:10 2019
+
+@author: Kdwing
+"""
+
+class GameStats:
+    '''Track statistics for Alien Invasion.'''
+    def __init__(self, ai_game):
+        '''Initialize statistics.'''
+        self.settings = ai_game.settings
+        self.reset_stats()
+    
+    def reset_stats(self):
+        '''Initialize statistics that can change during the game.'''
+        self.ship_left = self.settings.ship_limit
