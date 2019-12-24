496ec94ca86965ae0aadd2a04b019ff63052c035
Kak D-Wing <avehwing@gmail.com>
2019-12-24 08:56:25 +0800

    Update alien_invasion.py
    Initialize game statistics class.
    Added ship hit method to reduce ship left, clear all aliens and bullets and recenter the ship at bottom.

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,8 +6,12 @@ Created on Fri Dec 13 09:35:28 2019
 """
 
 import sys
+from time import sleep
+
 import pygame
+
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -31,6 +35,9 @@ class AlienInvasion:
         # self.settings.screen_width = self.screen.get_rect().width
         # self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
+        
+        # Create an instance to store game statistics
+        self.stats = GameStats(self)
         
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -133,7 +140,7 @@ class AlienInvasion:
         then update the positions of all aliens in the fleet.
         '''
         self._check_fleet_edges()
-        self.aliens.update()
+        self._ship_hit()
         
         # Look for alien-ship collisions.
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
@@ -180,6 +187,22 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+        
+    def _ship_hit(self):
+        '''Respnd to the ship being hit by an alien'''
+        # Decrement ships_left
+        self.stats.ship_left -= 1
+        
+        # Get rid of any remaining aliens and bullets
+        self.aliens.empty()
+        self.bullets.empty()
+        
+        # Create a new fleet and center the ship
+        self._create_fleet()
+        self.ship.center_ship()
+        
+        # Pause
+        sleep(0.5)
     
     def _update_screen(self):
         '''Update images on the screen and flip to the new screen.'''
