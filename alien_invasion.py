f76ed5bd7d6aa7b582d762c0e3368c77039f002d
Kak D-Wing <avehwing@gmail.com>
2019-12-26 05:50:31 +0800

    Update alien_invasion.py
    After the ship left reduced to zero, mouse cursos will become visible again.

--- alien_invasion.py
+++ alien_invasion.py
@@ -244,6 +244,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
     
     def _update_screen(self):
         '''Update images on the screen and flip to the new screen.'''
