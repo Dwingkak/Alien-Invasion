c2f24a57a3f2c4f51f44d9856a1c9b7e9d9ddce0
Kak D-Wing <avehwing@gmail.com>
2019-12-26 09:35:09 +0800

    Update button.py
    Added at argument to adjust button vertical position.

--- button.py
+++ button.py
@@ -9,7 +9,7 @@ import pygame.font
 
 class Button:
     
-    def __init__(self, ai_game, msg):
+    def __init__(self, ai_game, msg, position_x = "center", position_y = 0):
         '''Initialize button attribute'''
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
@@ -23,6 +23,13 @@ class Button:
         # Build the button's rect object and center it.
         self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = self.screen_rect.center
+        self.rect.y += position_y
+        if position_x == "center":
+            pass
+        elif position_x == "left":
+            self.rect.x -= 250
+        elif position_x == "right":
+            self.rect.x += 250
         
         # The button message needs to be prepped only once.
         self._prep_msg(msg)
@@ -32,7 +39,7 @@ class Button:
         self.msg_image = self.font.render(msg, True, self.text_color,
                                           self.button_color)
         self.msg_image_rect = self.msg_image.get_rect()
-        self.msg_image_rect.center = self.rect.center
+        self.msg_image_rect.center = self.rect.center       
         
     def draw_button(self):
         # Draw blank button and then draw message.
