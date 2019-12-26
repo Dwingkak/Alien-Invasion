56c55768e814607d21041fbaf717414187a14ab3
Kak D-Wing <avehwing@gmail.com>
2019-12-26 09:36:00 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ class Settings:
         self.ship_limit = 3
         
         # Bullet settings.
-        self.bullet_speed = 1.5
+        self.bullet_speed = 3.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
@@ -43,6 +43,24 @@ class Settings:
         self.ship_speed = 1.5
         self.bullet_speed = 3.0
         self.alien_speed = 1.0
+        
+        # fleet direction of 1 represent right; -1  represent left.
+        self.fleet_direction = 1
+    
+    def medium_difficulty_mode(self):
+        '''Set the game into medium difficulty'''
+        self.ship_speed = 1.5 * 1.1**4
+        self.bullet_speed = 3.0 * 1.1**4
+        self.alien_speed = 1.0 * 1.1 **4
+        
+        # fleet direction of 1 represent right; -1  represent left.
+        self.fleet_direction = 1
+    
+    def hard_difficulty_mode(self):
+        '''Set the game into hard difficulty'''
+        self.ship_speed = 1.5 * 1.1**9
+        self.bullet_speed = 3.0 * 1.1**9
+        self.alien_speed = 1.0 * 1.1 **9
         
         # fleet direction of 1 represent right; -1  represent left.
         self.fleet_direction = 1
