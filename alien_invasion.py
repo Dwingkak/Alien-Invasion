412da6b8fe446cf8536d0fd2d1b1e52e690da4c5
Kak D-Wing <avehwing@gmail.com>
2019-12-27 05:03:44 +0800

    Update alien_invasion.py
    Game screen now showing score at the top right corner.

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,6 +12,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -38,7 +39,9 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         
         # Create an instance to store game statistics
+        # and create a scoreboard
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
         
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -287,6 +290,10 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+        
+        # Draw the score information.
+        self.sb.show_score()
+        
         # Draw the play button if the game is inactive.
         if not self.stats.game_active and not self.stats.play_button_clicked:
             self.play_button.draw_button()
