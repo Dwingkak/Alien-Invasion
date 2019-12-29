2fe574e819e34c24796b271fa7eb6a166a47f036
Kak D-Wing <avehwing@gmail.com>
2019-12-30 06:15:38 +0800

    Update scoreboard.py
    Draw level display image below score

--- scoreboard.py
+++ scoreboard.py
@@ -24,6 +24,7 @@ class Scoreboard:
         # Prepare the initial score images.
         self.prep_score()
         self.prep_high_score()
+        self.prep_level()
         
     def prep_score(self):
         '''Turn the score into a rendered image.'''
@@ -49,6 +50,17 @@ class Scoreboard:
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
         
+    def prep_level(self):
+        '''Turn the level info into a rendered image.'''
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render(level_str, True,
+                            self.text_color, self.settings.bg_color)
+        
+        # Position of the level below the score.
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+        
     def check_high_score(self):
         '''Check to see if there's a new high score.'''
         if self.stats.score > self.stats.high_score:
@@ -56,9 +68,10 @@ class Scoreboard:
             self.prep_high_score()
         
     def show_score(self):
-        '''Draw score to the screen'''
+        '''Draw score and level to the screen'''
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
         
     
     
